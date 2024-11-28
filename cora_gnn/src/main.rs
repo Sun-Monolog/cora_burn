4a55fae1fbb5e895441b936a7cb16ff58870a3d1
AikHitori <65852831+sun-monolog@users.noreply.github.com>
2024-11-29 00:30:36 +0800

    Add files via upload

--- cora_gnn/src/main.rs
+++ cora_gnn/src/main.rs
@@ -1,3 +1,22 @@
+use burn::backend::wgpu::WgpuDevice;
+use burn::backend::{Autodiff, Wgpu};
+use burn::optim::AdamConfig;
+use cora_gnn::graph::model::GNNConfig;
+use cora_gnn::graph::train::{train, TrainingConfig};
+
 fn main() {
-    println!("Hello, world!");
+    // const CORA_NODE_NUM : usize = 2708;
+    // const CORA_EDGE_NUM : usize = 5429;
+    const CORA_NODE_EMBEDDING_LEN: usize = 1433;
+
+    type MyBacked = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBacked>;
+
+    let device = WgpuDevice::default();
+    let artifact_dir = "./tmp/cora";
+    train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(GNNConfig::new(CORA_NODE_EMBEDDING_LEN, CORA_NODE_EMBEDDING_LEN, CORA_NODE_EMBEDDING_LEN, 7), AdamConfig::new()),
+        device.clone()
+    );
 }
